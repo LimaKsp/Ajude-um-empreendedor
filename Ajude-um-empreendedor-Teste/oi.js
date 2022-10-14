6780242b8cae51dc85eb0106acf2f3a1de92fb55
Delson2000 <112819612+Delson2000@users.noreply.github.com>
2022-10-14 16:10:31 -0300

    Add files via upload
    slideer

---
+++ Ajude-um-empreendedor-Teste/oi.js
@@ -0,0 +1,13 @@
+let c = 1;
+// document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    document.getElementById("radio"+ c ).checked =  true;
+    c++;
+    if(c > 7){
+        c = 1;
+    }
+    
+},4000)
+
+
